8baf813f466addcb4a5ef7a4b87ddf27fc2cfb0f
Ivan <banivashka@gmail.com>
2018-02-26 13:42:32 +0300

    done
    --

---
+++ Ivan/Ivan/Class1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            string str;
+            bool run = true;
+            Arr MyArr = new Arr();
+            while (run)
+            {
+                Console.WriteLine("Нажмите 1, чтобы вывести меню на консоль");
+                str = Console.ReadLine();
+                switch (str)
+                {
+                    case "1":
+                        Console.WriteLine(@"Меню:
+1- Распечатка меню.
+2- Создать массив из файла
+3- создать массив с консоли
+4 - рандомно создать массив
+5 - напечатать массив
+6 - напечатать массив в файл
+7 - напечатать подматрицу
+8- Реализация функции 1: поменять столбцы в подматрице.
+9 - Реализация функции 2: поменять строки в подматрице.
+10 - Реализация функции 3: среднее арифметическое элементов, стоящих над побочной диагональю подматрицы.
+11 - Реализация функции 4: сумма элементов с четными индексами на диагонали подматрицы.
+12 - Реализация функции 5: медианы каждой строки.
+13- Завершение работы.");
+                        Console.WriteLine(" Выбкрите следующий пункт меню");
+                        break;
+
+                    case "2":
+                        try
+                        {
+                            Console.WriteLine("Input file name");
+                            str = Console.ReadLine();
+                            MyArr = new Arr(str);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+
+                    case "3":
+                        try
+                        {
+                            MyArr = new Arr(1);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+
+                    case "4":
+                        try
+                        {
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+
+                    case "5":
+                        try
+                        {
+                            MyArr.PrintToConsol();
+                        }                    
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+
+                    case "6":
+                        try
+                        {
+                            MyArr.PrintToFile();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+
+                    case "7":
+                        try
+                        {
+                            MyArr.PrintFragmToConsol();
+                        }
+                        catch (Exception e)
+                        { Console.WriteLine(e.Message); }
+                        break;
+
+                    case "8":
+                        try
+                        {
+                            Console.WriteLine("Inpute number of col1 and col2");
+                            n1 = Int32.Parse(Console.ReadLine());
+                            n2 = Int32.Parse(Console.ReadLine());
+                            MyArr.ChangeCol(n1, n2);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        break;
+
+                    case "9":
+                        try
+                        {
+                            Console.WriteLine("Inpute number of str1 and str2");
+                            n1 = Int32.Parse(Console.ReadLine());
+                            n2 = Int32.Parse(Console.ReadLine());
+                            MyArr.ChangeLine(n1, n2);
+                        }
+                        catch (Exception e)
+                        { 
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "10":
+                        Console.WriteLine("Average is" + MyArr.Average().ToString());
+                        break;
+                    case "11":
+                        Console.WriteLine("Sum is" + MyArr.SumOfEvenElem().ToString());
+                        break;
+                    case "12":
+                        for (int i = 0; i < 11; i++)
+                            Console.WriteLine(MyArr.FindMediana()[i].ToString());
+                        break;
+                    case "13":
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неверно введен пункт меню.Введите правильный номер!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Ivan/Ivan/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyNamespace
+{
+    public abstract class BaseArr
+    {
+        protected static int NumOfLines = 11;
+        protected double[][] MyArr;
+        protected int s1, st1, min_lenght_of_line = 18;
+        protected BaseArr()
+        {
+            int temp;
+            Random rand = new Random();
+            MyArr = new double[NumOfLines][];
+            for (int i = 0; i < 11; i++)
+            {
+                temp = rand.Next(9, 18);
+                if (min_lenght_of_line > temp)
+                    min_lenght_of_line = temp;
+                MyArr[i] = new double[temp];
+                for (int j = 0; j < MyArr[i].Length; j++)
+                    MyArr[i][j] = rand.Next(100) + rand.NextDouble();
+            }
+            s1 = rand.Next(2, 10);
+            st1 = rand.Next(2, min_lenght_of_line - 2);
+            
+        }
+        protected BaseArr(int k) 
+        {
+            int temp = 0;
+            MyArr = new double[NumOfLines][];
+            Console.WriteLine("Input num of elements in 11 lines");
+            for (int i = 0; i < 11; i++)
+            {
+                temp = Int32.Parse(Console.ReadLine());
+                if (temp < 9 || temp > 18)
+                    throw new ApplicationException("Wrong number");
+                else
+                {
+                    if (min_lenght_of_line > temp)
+                        min_lenght_of_line = temp;
+                    MyArr[i] = new double[temp];
+                }
+            }
+            Console.WriteLine("Input numbers:");
+            for (int i = 0; i < NumOfLines; i++)
+                for (int j = 0; j < MyArr[i].Length; j++)
+                    MyArr[i][j] = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Input s1");
+            temp = Int32.Parse(Console.ReadLine());
+            if (temp < 1 || temp > 11)
+                throw new ApplicationException("Wrong number");
+            else
+                s1 = temp;
+            Console.WriteLine("Input st1");
+            temp = Int32.Parse(Console.ReadLine());
+            if (temp < 1 || temp > min_lenght_of_line)
+                throw new ApplicationException("Wrong number");
+            else
+                st1 = temp;
+
+        }
+        protected BaseArr(string FileName) 
+        {
+            using (StreamReader file = new StreamReader("1.txt"))
+            {
+                int temp = 0;
+                MyArr = new double[NumOfLines][];
+                for (int i = 0; i < 11; i++)
+                {
+                    temp = Int32.Parse(file.ReadLine());                         //считываем количество элементов в строке
+                    if (temp < 9 || temp > 18)
+                        throw new ApplicationException("Wrong number");
+                    else
+                    {
+                        if (min_lenght_of_line > temp)
+                        min_lenght_of_line = temp;
+                        MyArr[i] = new double[temp];                                //считываем элементы
+                        for (int j = 0; j < temp; j++)
+                            MyArr[i][j] = Double.Parse(file.ReadLine());
+                    }
+                }
+                temp = Int32.Parse(file.ReadLine());
+                if (temp < 1 || temp > 11)
+                    throw new ApplicationException("Wrong number");
+                else
+                    s1 = temp;
+                temp = Int32.Parse(file.ReadLine());
+                if (temp < 1 || temp > min_lenght_of_line)
+                    throw new ApplicationException("Wrong number");
+                else
+                    st1 = temp;
+            }
+        }
+        public abstract void ChangeCol(int Col1, int Col2);
+        public abstract void ChangeLine(int Line1, int Line2);
+        public abstract double[] FindMediana();
+        public abstract double SumOfEvenElem();
+        public abstract double Average();
+        public abstract void PrintToConsol();
+        public abstract void PrintFragmToConsol();
+        public abstract void PrintToFile();
+    }
+
+    public class Arr : BaseArr
+    {
+        public Arr() : base() { }
+        public Arr(int i) : base(i) { }
+        public Arr(string FileName) : base(FileName) { }
+        public override void PrintToConsol()
+        {
+            for (int i = 0; i < NumOfLines; i++)
+            {
+                for (int j = 0; j < MyArr[i].Length - 1; j++)
+                {
+                    Console.Write("{0:0.00}", MyArr[i][j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine("{0:0.00}", MyArr[i][MyArr[i].Length - 1]);
+            }
+        }
+        public override void PrintFragmToConsol()
+        {
+            for (int i = 1; i < s1 + 1; i++)
+            {
+                for (int j = 2; j < st1 + 1; j++)
+                {
+                    Console.Write("{0:0.00}", MyArr[i][j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine("{0:0.00}", MyArr[i][st1 + 1]);
+            }
+        }
+        public override void PrintToFile()
+        {
+            string name;
+            Console.WriteLine("Input filename");
+            name = Console.ReadLine();
+            StreamWriter file = new StreamWriter(name);
+            for (int i = 0; i < NumOfLines; i++)
+            {
+                for (int j = 0; j < MyArr[i].Length - 1; j++)
+                    file.Write(MyArr[i][j].ToString() + " ");
+                file.WriteLine(MyArr[i][MyArr[i].Length - 1]);
+            }
+            file.Close();
+        }
+        public override void ChangeCol(int Col1, int Col2)               //добавить исключ
+        {
+            double temp;
+            if (st1 + 1 < Col1 || st1 + 1 < Col2 || Col2 < 2 || Col1 < 2)
+                Console.WriteLine("Too much num of columns for this fragment");
+            else
+            {
+                for (int i = 1; i < s1 + 1; i++)
+                {
+                    temp = MyArr[i][Col1];
+                    MyArr[i][Col1] = MyArr[i][Col2];
+                    MyArr[i][Col2] = temp;
+                }
+            }
+        }
+        public override void ChangeLine(int Line1, int Line2)               //добавить исключ и дописать как колонки, чтоб работала с фрагментом
+        {
+            double temp;
+            if (Line1 < 1 || Line2 < 1 || Line1 > s1 || Line2 > s1)
+                Console.WriteLine("Too much num of columns for this fragment");
+            else
+            {
+                for (int i = 2; i < 2 + st1; i++)
+                {
+                    temp = MyArr[Line1][i];
+                    MyArr[Line1][i] = MyArr[Line2][i];
+                    MyArr[Line2][i] = temp;
+                }
+            }
+        }
+        public override double[] FindMediana()
+        {
+            double []res = new double [11];
+            double sum = 0;
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < MyArr[i].Length; j++)
+                    sum += MyArr[i][j];
+                sum = sum / NumOfLines;
+                res[i] = MyArr[i][0];
+                for (int j = 0; j < MyArr[i].Length; j++)
+                    if (Math.Abs(sum - MyArr[i][j]) < Math.Abs(sum - res[i]))
+                        res[i] = MyArr[i][j];
+                sum = 0;
+            }
+            return res;
+        }
+        public override double SumOfEvenElem()
+        {
+            int len = s1;
+            double sum = 0;
+            if (st1 < len)
+                len = st1;
+            for (int i = 1; i < len + 1; i += 2)
+                sum += MyArr[i][i + 1];
+            return sum;
+        }
+        public override double Average()
+        {
+            int len = s1;
+            double sum = 0, num = 0;
+            if (st1 < len)
+                len = st1;
+            for (int i = s1; i > 0; i--)
+            {
+                for (int j = 2; j < s1 + 2 - i; j++)
+                {
+                    sum += MyArr[i][j];
+                    num++;
+                }
+            }
+            return sum / num;
+        }
+
+    }
+}
